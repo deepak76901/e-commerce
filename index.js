d17bad711cdc5fc571ab355eeedb1f9d2f4ecae3
deepak76901 <deepakb76901@gmail.com>
2024-04-10 21:35:08 +0530

    minor changes

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ import { router as authRouter } from "./routes/auth.router.js";
 import { router as cartRouter } from "./routes/cart.router.js";
 import { router as orderRouter } from "./routes/order.router.js";
 import morgan from "morgan";
+import path from "path"
 
 dotenv.config({
   path: "./.env",
 });
 
 const app = express();
+const __dirname = path.resolve()
+
 
 // Middlewares
 app.use(express.json());
@@ -48,6 +51,13 @@ app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use("/cart", cartRouter);
 app.use("/orders", orderRouter);
+
+
+app.use(express.static(path.join(__dirname,"/client/build")))
+
+app.get("*",(req,res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
