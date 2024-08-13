9cb3bf12b07ae89526188012970908bfe570ec20
deepak76901 <deepakb76901@gmail.com>
2024-08-13 07:46:02 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       '/api': {
         target: 'https://e-commerce-api-12.vercel.app/', // Replace with your backend API URL
         changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''), // Optional: rewrite the path
+        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: rewrite the path
         secure:true
       },
     },
